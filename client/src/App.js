e53c7a6f5bdd9fae2618b434b3ceca5fd3402246
Palak <behlpari4@gmail.com>
2026-02-18 23:45:26 +0530

    Setup App with Material-UI Container and Grid

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+
+import { getPosts } from './actions/posts';
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
+
   return (
-    <div>
-      <h1>Memora</h1>
-      <p>App is running!</p>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">Memora</Typography>
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+              {/* Posts component will go here */}
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              {/* Form component will go here */}
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 };
 
