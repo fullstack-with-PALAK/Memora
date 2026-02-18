256df2dce926b61f6054b466072175e4976969c8
Palak <behlpari4@gmail.com>
2026-02-18 23:33:06 +0530

    Connect posts routes to main server

--- server/index.js
+++ server/index.js
@@ -3,12 +3,17 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js';
+
 const app = express();
 
 // Middleware
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
+
+// Routes
+app.use('/posts', postRoutes);
 
 app.get('/', (req, res) => {
   res.send('Memora API is running');
