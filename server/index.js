fe679a2334a6fc102a8de30195ac271de753b938
Palak <behlpari4@gmail.com>
2026-02-18 23:30:54 +0530

    Add CORS and body-parser middleware

--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 
 const app = express();
+
+// Middleware
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Memora API is running');
